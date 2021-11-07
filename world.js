173396d7016df488e97f32f867ed0a73dd9a69be
Giovanne Pinto <pintogiovanne@gmail.com>
2021-11-07 17:31:03 -0500

    Edited Files

---
+++ world.js
@@ -0,0 +1,51 @@
+"use strict";
+window.onload = function(){
+    
+    var btn = document.getElementById('lookup');
+    var city = document.getElementById('cities');
+    var search = document.getElementById('country');
+    var url = 'http://localhost/info2180-lab5/world.php?country=';
+    var result = document.getElementById('result');
+
+    
+    btn.addEventListener('click',function(event){
+        event.preventDefault();
+        let query_element =  search.value;
+        let request = new URL(url+query_element+'&context=') ;
+        fetch (request)
+            .then(response => {
+                if (response.ok){
+                    return response.text()
+                } 
+                else{
+                    return Promise.reject('something went wrong')
+                }
+        })
+            .then (function(data){
+            result.innerHTML = data;
+        })
+            .catch (error => console.log('There was an error: ' + error))
+    
+})
+
+    city.addEventListener('click',function(event){
+        event.preventDefault();
+        let query_element = search.value;
+        let request = new URL(url + query_element + '&context=cities');
+        fetch (request)
+            .then(response => {
+                if (response.ok){
+                    return response.text()
+                } 
+                else{
+                    return Promise.reject('something went wrong')
+                }
+        })
+            .then (function(data){
+            result.innerHTML = data;
+        })
+            .catch (error => console.log('There was an error: ' + error))
+    })
+
+
+    }
--- world.php
+++ world.php
@@ -1,17 +1,65 @@
+
+<?php if ($_SERVER['REQUEST_METHOD']=== 'GET'):?>
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
+$country = filter_input(INPUT_GET, 'country',FILTER_SANITIZE_STRING,
+FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH);
+
+$city = filter_input(INPUT_GET, 'context',FILTER_SANITIZE_STRING,
+FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH);
+
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+
+$all_cities = $conn->query("SELECT cities.name, cities.district, cities.population 
+FROM cities 
+JOIN countries 
+ON cities.country_code = countries.code WHERE countries.name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$city_results = $all_cities->fetchAll(PDO::FETCH_ASSOC)
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
+
+<?php if ($city == ''):?>
+
+<table>
+<tr>
+  <th>Name</th>
+  <th>Continent</th>
+  <th>Independence</th>
+  <th>Head of State</th>
+</tr>
+
+  <?php foreach ($results as $row): ?>
+  <tr>
+    <td><?= $row['name'] ?></td>
+    <td><?= $row['continent'] ?></td>
+    <td><?= $row['independence_year'] ?></td>
+    <td><?= $row['head_of_state'] ?></td>
+  </tr>
+  <?php endforeach; ?>
+
+</table>
+<?php elseif($city == 'cities'): ?>
+
+<table>
+  <tr>
+    <th>Name</th>
+    <th>District</th> 
+    <th>Population</th> 
+  </tr>
+  <?php foreach ($city_results as $row): ?>
+  <tr>
+    <td><?= $row['name'] ?></td>
+    <td><?= $row['district'] ?></td>
+    <td><?= $row['population'] ?></td>
+  </tr>
+  <?php endforeach; ?>
+<?php endif ?>
+<?php endif ?>
